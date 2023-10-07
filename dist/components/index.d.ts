c98f34d5c44bbd45b7e56d7a2ec8486e0245b286
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-10-07 00:36:13 -0400

    Delete dist directory

--- dist/components/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { ClientProfile } from "./ClientCard/ClientProfile";
-export { ClientCardLG } from "./ClientCard/ClientCardLG";
-export { ClientCardSM } from "./ClientCard/ClientCardSM";
-export { ClientList } from "./ClientList";
-export { Leaderboard } from "./Leaderboard";
-export { UserList } from "./UserList";
